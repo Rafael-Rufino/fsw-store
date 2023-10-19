d3e96e417989dfa682bb2941cdedf9f15c7d98e0
rafael rufino <rafaelrufino.52@email.com>
2023-10-19 17:13:30 -0300

    feat: add  discont banner to home  page

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-
-export default function Home() {
-  return <div>home</div>;
-}
-git config --global user.name "rafael rufino"
-git config --global user.email "rafaelrufino.52@email.com"
