24a2cded789401b6ce8949dab0b776c92a26ba7c
rafael rufino <rafaelrufino.52@email.com>
2023-10-25 18:15:42 -0300

    style: ajust style

--- src/components/ui/cart-item.tsx
+++ src/components/ui/cart-item.tsx
@@ -27,51 +27,54 @@ const CartItem = ({ product }: CartItemProps) => {
     removeProductFromCart(product.id);
   };
   return (
-    <div className="flex items-center justify-between ">
-      <div className=" flex h-[77px] w-[77px] items-center justify-center rounded-lg bg-accent">
-        <Image
-          src={product.imageUrls[0]}
-          width={0}
-          height={0}
-          sizes="100vw"
-          alt={product.name}
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
-        />
+    <div className="flex justify-between   ">
+      <div className="flex items-center gap-5 ">
+        <div className=" flex h-[77px] w-[77px] items-center justify-center rounded-lg bg-accent">
+          <Image
+            src={product.imageUrls[0]}
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt={product.name}
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          />
+        </div>
+
+        <div className="flex flex-col ">
+          <h3 className="text-xs font-semibold">{product.name}</h3>
+          <div className="flex items-center gap-2">
+            <h4 className="text-sm font-bold">
+              {formattedPrice(product.totalPrice)}
+            </h4>
+
+            {product.discountPercentage > 0 && (
+              <span className="text-xs line-through opacity-75">
+                {formattedPrice(Number(product.basePrice))}
+              </span>
+            )}
+          </div>
+          <div className="mt-2 flex items-center gap-1">
+            <Button
+              size="icon"
+              variant="outline"
+              className="h-8 w-8"
+              onClick={handleDecreaseProductQuantity}
+            >
+              <ArrowLeftIcon size={16} />
+            </Button>
+            <span className="tetx-xs px-2">{product.quantity}</span>
+            <Button
+              size="icon"
+              variant="outline"
+              className="h-8 w-8"
+              onClick={handleIncreaseProductQuantity}
+            >
+              <ArrowRightIcon size={16} />
+            </Button>
+          </div>
+        </div>
       </div>
 
-      <div className="flex flex-col ">
-        <h3 className="text-xs font-semibold">{product.name}</h3>
-        <div className="flex items-center gap-2">
-          <h4 className="text-sm font-bold">
-            {formattedPrice(product.totalPrice)}
-          </h4>
-
-          {product.discountPercentage > 0 && (
-            <span className="text-xs line-through opacity-75">
-              {formattedPrice(Number(product.basePrice))}
-            </span>
-          )}
-        </div>
-        <div className="mt-2 flex items-center gap-1">
-          <Button
-            size="icon"
-            variant="outline"
-            className="h-8 w-8"
-            onClick={handleDecreaseProductQuantity}
-          >
-            <ArrowLeftIcon size={16} />
-          </Button>
-          <span className="tetx-xs px-2">{product.quantity}</span>
-          <Button
-            size="icon"
-            variant="outline"
-            className="h-8 w-8"
-            onClick={handleIncreaseProductQuantity}
-          >
-            <ArrowRightIcon size={16} />
-          </Button>
-        </div>
-      </div>
       <Button
         size="icon"
         variant="outline"
