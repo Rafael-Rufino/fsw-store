e83c26f89e3303b68b54271b8f4f4c1dc8cab048
rafael rufino <rafaelrufino.52@email.com>
2023-10-25 14:21:42 -0300

    feat: add PriceRow component

---
+++ src/components/ui/priceRow.tsx
@@ -0,0 +1,28 @@
+import { formattedPrice } from "@/utils/formattedPrice";
+import { Separator } from "./separator";
+import { twMerge } from "tailwind-merge";
+
+interface PriceRowProps {
+  label: string;
+  value: number;
+  className?: string;
+}
+
+const PriceRow = ({ label, value, className, ...props }: PriceRowProps) => {
+  const rowClasses = twMerge(
+    "mt-4 flex items-center justify-between text-sm",
+    className,
+  );
+
+  return (
+    <>
+      <Separator />
+      <div className={rowClasses} {...props}>
+        <small>{label}</small>
+        <small>{formattedPrice(value)}</small>
+      </div>
+    </>
+  );
+};
+
+export default PriceRow;
