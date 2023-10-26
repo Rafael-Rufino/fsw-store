e244ff1cda2e2db86dacaec3a6ef607c376b11b4
rafael rufino <rafaelrufino.52@email.com>
2023-10-26 15:19:21 -0300

    feat: add DealsPage and DealsList componet

---
+++ src/app/deals/components/deals-list.tsx
@@ -0,0 +1,28 @@
+import ProductItem from "@/components/ui/product-item";
+import computeProductTotalPrice from "@/helpers/product";
+import { prismaClient } from "@/lib/prisma";
+
+const DealsList = async () => {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+  if (!deals) return null;
+  return (
+    <div className="grid grid-cols-2 gap-8  md:grid-cols-3 lg:md:grid-cols-4">
+      {deals.map((product) => {
+        return (
+          <ProductItem
+            key={product.id}
+            product={computeProductTotalPrice(product)}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default DealsList;
---
+++ src/app/deals/page.tsx
@@ -0,0 +1,21 @@
+import { Badge } from "@/components/ui/badge";
+
+import { ShapesIcon } from "lucide-react";
+import DealsList from "./components/deals-list";
+
+const Deals = async () => {
+  return (
+    <div className="flex flex-col gap-5 p-5 ">
+      <Badge
+        variant="outline"
+        className=" w-fit gap-1 border-2 border-primary px-3 py-2 text-base uppercase"
+      >
+        <ShapesIcon size={16} />
+        Ofertas
+      </Badge>
+      <DealsList />
+    </div>
+  );
+};
+
+export default Deals;
